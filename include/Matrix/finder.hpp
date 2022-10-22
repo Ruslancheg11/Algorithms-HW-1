d09ffdba48b9bc84b6b62890e324a54d6a5cd44d
RuslanWin10 <ruslan.sorokin.yyt@mail.ru>
2022-10-22 07:59:33 +0300

    feat: add staircase search

--- include/Matrix/finder.hpp
+++ include/Matrix/finder.hpp
@@ -2,13 +2,16 @@
 #define MATRIX_FINDER_HPP
 /* START OF INCLUDING SECTION */
 #include <generator.hpp>
+#include <iostream>
 /* END OF INCLUDING SECTION */
 
 
 auto linear_search(const TMatrix& Matrix, const size_t Value) -> bool;
 
+auto vert_binary_search(const TMatrix& Matrix, const size_t Value) -> bool;
+
 auto binary_search(const TMatrix& Matrix, const size_t Value) -> bool;
 
-auto two_pointer_search(const TMatrix& Matrix, const size_t Value) -> bool;
+auto staircase_search(const TMatrix& Matrix, const size_t Value) -> bool;
 
 #endif // MATRIX_FINDER_HPP
